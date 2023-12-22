d301e097c5f4c4d040529d25d909c65967ae942f
tejasravi12 <152912875+tejasravi12@users.noreply.github.com>
2023-12-21 17:31:48 -0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -5,43 +5,59 @@ import java.awt.Graphics;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 
-class Student {
-  private int y;
-  private int x;
-  private int yVel;
-  private Image image;
+public class Student extends Block implements Collideable<Block>{
+  private Image image1;
+  private Image image2;
+  private Image image3;
+  private int costume = 0;
 
   public Student() {
-    this(50, 500);
+    super(50, 500);
   }
 
   public Student(int x, int y) {
-    this.y = y;
-    this.x = x;
+    super(x,y);
     try {
-      URL url = getClass().getResource("pixelMan.png");
-      image = ImageIO.read(url);
+      URL url = getClass().getResource("ForwardRun.png");
+      image1 = ImageIO.read(url);
     } catch (Exception e) {
     }
-  }
+    try {
+      URL url = getClass().getResource("BackRun.png");
+      image2 = ImageIO.read(url);
+    }catch (Exception e) {
+        }try {
+      URL url = getClass().getResource("Jump.png");
+      image3 = ImageIO.read(url);
+    }catch (Exception e) {
+    }
 
-  public int getY() {
-    return y;
-  }
-
-  public void setY(int y) {
-    this.y = y;
-  }
-
-  public int getYVel() {
-    return yVel;
-  }
-
-  public void setYVel(int yVel) {
-    this.yVel = yVel;
   }
 
   public void draw(Graphics window) {
-    window.drawImage(image, x, y - 60, 170, 70, null);
+    costume++;
+    if(getYVel()!=0){
+      window.drawImage(image3, getX(), getY(), getWidth(), getHeight(), null);
+    }
+    else if (costume > 40) {
+      window.drawImage(image1, getX(),  getY(), getWidth(), getHeight(), null);
+      if(costume > 80){
+        costume = 0;
+      }
+    }
+    else {
+      window.drawImage(image2, getX(),  getY(), 80, 80, null);
+    }
+  }
+  public boolean didCollideLeft(Block obj){
+    return getX() <= obj.getX() + obj.getWidth() && getX() >= obj.getX()
+      && getY() + getHeight() >= obj.getY() && getY() <= obj.getY() + obj.getHeight();
+  }
+  public boolean didCollideRight(Block obj){
+    return getX() +getWidth()>= obj.getX() && getX() + getWidth() <= obj.getX() + obj.getWidth()
+      && getY() + getHeight() >= obj.getY() && getY() <= obj.getY() + obj.getHeight();
+  }
+  public boolean didCollide(Block obj){
+    return didCollideLeft(obj) || didCollideRight(obj);
   }
 }
